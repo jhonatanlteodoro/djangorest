0fd30b317d6c711949c0c8014d5dbc56dfb95f77
Jhonatan Teodoro <mollitiamteodoro@gmail.com>
2019-01-24 20:44:27 -0200

    Criado model para produto

--- api/models.py
+++ api/models.py
@@ -1,3 +1,15 @@
+from datetime import datetime
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    create_on = models.DateTimeField(default=datetime.now)
+
+    CATEGORIES = (
+        ('notebooks', 'Notebooks'),
+        ('eletrodomesticos', 'Eletrodomésticos'),
+        ('moveis', 'Móveis'),
+        ('smartphone', 'Smartphone')
+    )
+    category = models.CharField(max_length=16, choices=CATEGORIES)
