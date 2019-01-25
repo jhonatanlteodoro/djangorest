c5ed384a7ce062562f1db9fcbd3fad628a14a3df
Jhonatan Teodoro <mollitiamteodoro@gmail.com>
2019-01-25 12:46:12 -0200

    custom admin view

--- api/admin.py
+++ api/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from .models import Product
 
-admin.site.register(Product)
+# admin.site.register(Product)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category'
+    )
+    list_filter = ('created_on',)
+    ordering = ('created_on',)
+    search_fields = ('name',)
